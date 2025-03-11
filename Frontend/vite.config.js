554518035dbf863f7114b63bb96c0c8319d93d2d
Rachitlad24 <rachitlad.it21@scet.ac.in>
2025-03-11 14:02:44 +0530

    vite change

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react({ fastRefresh: false })],
   resolve: {
@@ -10,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: process.env.PORT || 5173,
+    host: "0.0.0.0",
+  }
 })
